947712d32346058ca94d17f18da979aa57d6e031
Bean900 <n.nikisch@gmx.de>
2016-10-12 08:59:15 +0200

    Bugfix

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "landrive/server"
 
 func main(){
 	flag.String("Port", "1234", "The Port of the Server")
-	flag.String("ServerKey","src/de/vorlesung/projekt/landrive/server/server.key","The Server Key File")
-	flag.String("ServerCrt", "src/de/vorlesung/projekt/landrive/server/server.crt", "The Server crt File")
+	flag.String("ServerKey","src/landrive/server/server.key","The Server Key File")
+	flag.String("ServerCrt", "src/landrive/server/server.crt", "The Server crt File")
 	flag.Parse()
 	
 	
--- server/fileserver.go
+++ server/fileserver.go
@@ -1,16 +1,17 @@
-package server;
+package server
 
 import (
+	"flag"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
-	"flag"
 )
 
-func StartFileserver() { 
+func StartFileserver() {
 	log.Println("Server Startet")
 	http.HandleFunc("/hello", HelloServer)
-	err := http.ListenAndServeTLS(":"+flag.Lookup("Port").Value.String(), flag.Lookup("ServerKey").Value.String(), flag.Lookup("ServerCrt").Value.String(), nil)
+	err := http.ListenAndServeTLS(":"+flag.Lookup("Port").Value.String(), flag.Lookup("ServerCrt").Value.String(), flag.Lookup("ServerKey").Value.String(), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
