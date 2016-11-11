b5eab947371b9102d6492250db7f1f59cda9e2b6
Bean900 <n.nikisch@gmx.de>
2016-11-11 14:50:26 +0100

    Test Methoden geschrieben aber nicht gefüllt

--- server/fileserver_test.go
+++ server/fileserver_test.go
@@ -1,9 +1,63 @@
 package server
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
-func TestIsServerUp(t *testing.T) {
-    assert.Equal(t, 123, 123, "Server should be up")
+
+//Die Web-Seite soll nur per HTTPS erreichbar sein.
+func TestPingPage(t *testing.T) {
+
 }
+
+//Der Zugang soll durch Benutzernamen und Passwort geschuëtzt werden.
+func TestAcess(t *testing.T) {
+
+}
+
+//Die Passwoerter duërfen nicht im Klartext gespeichert werden.
+func TestPassword(t *testing.T) {
+
+}
+
+//Zur weiteren Identifikation des Nutzers soll ein Session-ID Cookie ver- wendet werden.
+func TestCookie(t *testing.T) {
+
+}
+
+// Neue Nutzer sollen selbst einen Zugang anlegen ko ̈nnen.
+func TestCreatUser(t *testing.T) {
+
+}
+
+// Ein Nutzer soll sein Passwort  aendern koennen.
+func TestChangePassword(t *testing.T) {
+
+}
+
+
+// Es soll mo ̈glich sein, Dateien ”hochzuladen“
+func TestSaveFile(t *testing.T) {
+
+}
+
+// Es soll mo ̈glich sein, Dateien ”herunterzuladen“
+func TestDownloadFile(t *testing.T) {
+
+}
+
+// Es soll mo ̈glich sein, Dateien ”herunterzuladen“ ueber wget
+func TestDownloadFileWGET(t *testing.T) {
+
+}
+
+//Es mo ̈glich sein, Dateien zu lo ̈schen.
+func TestDeletFile(t *testing.T) {
+
+}
+
+//Auch in diese Unterordner sollen sich Dateien laden lassen.
+func TestCreateFolder(t *testing.T) {
+
+}
+ 
